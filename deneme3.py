50317c414ae566828be69e97fea21430b6ae689d
efe3535 <a.efe.akyazi@gmail.com>
2020-11-24 21:59:05 +0300

    modified:   deneme3.py

--- deneme3.py
+++ deneme3.py
@@ -1,17 +1,38 @@
 user = input("admin/user ")
-while True:
-    if user == "admin":
-        baslangic = input("başlanacak sayı")
-        bitis = input("kaça kadar?")
-        atlama = input("kaç atlamalıyız")
-        
-        for _ in range(int(baslangic),int(bitis)+1,int(atlama)):
-            print(_)
-        break
-    elif user == "user":
-        sayi4 = input("kaça kadar?")
-        for _ in range(int(sayi4)+1):
-            print(_)
-        break
-    else:
-        print("lütfen dogru giriniz")
+
+parola = "1234"
+
+haksayac = 0
+
+if user == "admin":
+    while True:
+        sifre = input("parolayı giriniz")
+        if sifre == parola:
+            while True:
+                if user == "admin":
+                    baslangic = input("başlanacak sayı")
+                    bitis = input("kaça kadar?")
+                    atlama = input("kaç atlamalıyız")
+
+                    for _ in range(int(baslangic), int(bitis)+1, int(atlama)):
+                        print(_)
+                    break
+                elif user == "user":
+                    sayi4 = input("kaça kadar?")
+                    for _ in range(int(sayi4)+1):
+                        print(_)
+                    break
+                else:
+                    print("lütfen dogru giriniz")
+        else:
+            if haksayac != 3:
+                haksayac += 1
+                print(f"Yanlış girdiniz. {4-haksayac} hakkınız kaldı")
+            elif haksayac == 3:
+                print("haklarınız bitti")
+                break
+
+elif user == "user":
+    sayi4 = input("kaça kadar?")
+    for _ in range(int(sayi4)+1):
+        print(_)
