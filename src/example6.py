67075b0ddb730798423d8c051266eceb4122a232
efe3535 <a.efe.akyazi@gmail.com>
2021-03-22 11:34:16 +0300

    a

--- src/example6.py
+++ src/example6.py
@@ -1,1 +1,11 @@
-    
+import os
+
+def main():
+    if os.name == "posix":
+        print("Linux user!")
+    if os.name == "darwin":
+        print("macOS user!")
+    if os.name == "nt":
+        print("Windows user!")
+if __name__ == "__main__":
+    main()
