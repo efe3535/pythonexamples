bf317da0389f2de39f94656aa3accec400f508e0
Ahmet Efe AKYAZI <a.efe.akyazi@gmail.com>
2021-03-26 15:07:41 +0300

    example7-arguments.py update! 🆙 Added new argument --allcolors to list colors.

--- src/example7-arguments.py
+++ src/example7-arguments.py
@@ -7,6 +7,7 @@ helpfile = """
 ======================
 --help : Lists commands.
 --color : Requires one argument, prints 'Hello World' in color selected. 
+--allcolors : prints 'Hello World' with all colors.
 Colors:
 
 BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE
@@ -21,40 +22,57 @@ if len(sys.argv) != 1:
         if nextcolor == False:
             if argument == "--help":
                 print(helpfile)
+            elif argument == "--allcolors":
+                print(Fore.BLACK, "Hello World", Fore.RESET)
+                print(Fore.RED, "Hello World", Fore.RESET)
+                print(Fore.GREEN, "Hello World", Fore.RESET)
+                print(Fore.YELLOW, "Hello World", Fore.RESET)
+                print(Fore.BLUE, "Hello World", Fore.RESET)
+                print(Fore.MAGENTA, "Hello World", Fore.RESET)
+                print(Fore.CYAN, "Hello World", Fore.RESET)
+                print(Fore.WHITE, "Hello World", Fore.RESET)
             elif argument == "--color":
                 if(sys.argv[int(sys.argv.index(argument)) + 1] == "BLACK"):
                     print(Fore.BLACK + "Hello World" + Fore.RESET)
                     nextcolor = not nextcolor
-                
+
                 if(sys.argv[int(sys.argv.index(argument)) + 1] == "RED"):
                     print(Fore.RED + "Hello World" + Fore.RESET)
                     nextcolor = not nextcolor
-                
+
                 if(sys.argv[int(sys.argv.index(argument)) + 1] == "GREEN"):
                     print(Fore.GREEN + "Hello World" + Fore.RESET)
                     nextcolor = not nextcolor
-                
-                if(sys.argv[int(sys.argv.index(argument)) + 1] == "YELLOW" ):
-                    print(Fore.YELLOW + "Hello World" + Fore.RESET )
+
+                if(sys.argv[int(sys.argv.index(argument)) + 1] == "YELLOW"):
+                    print(Fore.YELLOW + "Hello World" + Fore.RESET)
                     nextcolor = not nextcolor
-                
-                if(sys.argv[int(sys.argv.index(argument)) + 1] == "BLUE" ):
-                    print(Fore.BLUE + "Hello World" + Fore.RESET )
+
+                if(sys.argv[int(sys.argv.index(argument)) + 1] == "BLUE"):
+                    print(Fore.BLUE + "Hello World" + Fore.RESET)
                     nextcolor = not nextcolor
-                
-                if(sys.argv[int(sys.argv.index(argument)) + 1] == "MAGENTA" ):
-                    print(Fore.MAGENTA + "Hello World" + Fore.RESET )
+
+                if(sys.argv[int(sys.argv.index(argument)) + 1] == "MAGENTA"):
+                    print(Fore.MAGENTA + "Hello World" + Fore.RESET)
                     nextcolor = not nextcolor
-                
-                if(sys.argv[int(sys.argv.index(argument)) + 1] == "CYAN" ):
-                    print(Fore.CYAN + "Hello World" + Fore.RESET )
+
+                if(sys.argv[int(sys.argv.index(argument)) + 1] == "CYAN"):
+                    print(Fore.CYAN + "Hello World" + Fore.RESET)
                     nextcolor = not nextcolor
-                
-                if(sys.argv[int(sys.argv.index(argument)) + 1] == "WHITE" ):
-                    print(Fore.WHITE + "Hello World" + Fore.RESET )
+
+                if(sys.argv[int(sys.argv.index(argument)) + 1] == "WHITE"):
+                    print(Fore.WHITE + "Hello World" + Fore.RESET)
                     nextcolor = not nextcolor
         elif argument == "--help":
             print(helpfile)
-
+        elif argument == "--allcolors":
+            print(Fore.BLACK, "Hello World", Fore.RESET)
+            print(Fore.RED, "Hello World", Fore.RESET)
+            print(Fore.GREEN, "Hello World", Fore.RESET)
+            print(Fore.YELLOW, "Hello World", Fore.RESET)
+            print(Fore.BLUE, "Hello World", Fore.RESET)
+            print(Fore.MAGENTA, "Hello World", Fore.RESET)
+            print(Fore.CYAN, "Hello World", Fore.RESET)
+            print(Fore.WHITE, "Hello World", Fore.RESET)
 else:
     print(helpfile)
